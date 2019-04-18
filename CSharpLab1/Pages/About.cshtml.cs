64a49b90d93d9d0803dd406290065c21f1424b53
zdzulak <43354865+zdzulak@users.noreply.github.com>
2019-04-18 10:17:19 -0400

    first upload

---
+++ CSharpLab1/Pages/About.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace CSharpLab1.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
+        }
+    }
+}
